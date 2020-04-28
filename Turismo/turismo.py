bf2788e7c3a6e7de9dd70ee35cee85f100e86224
AbrahamPlazola <64374554+AbrahamPlazola@users.noreply.github.com>
2020-04-28 10:11:10 -0600

    Añadido funciones de indice

--- Turismo/turismo.py
+++ Turismo/turismo.py
@@ -1,2 +1,14 @@
 destinos = ["Paris, Francia", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egipto"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+def obtenerIndiceDestino(destino):
+    indiceDestino = destinos.index(destino)    
+    return indiceDestino
+
+def locacionViajero(viajero):
+    destinoViaje = test_traveler[1]
+    indiceDestinoViajero = obtenerIndiceDestino(destinoViaje)
+    return indiceDestinoViajero
+
+test_indiceDestino = locacionViajero(test_traveler)
+print(test_indiceDestino)
