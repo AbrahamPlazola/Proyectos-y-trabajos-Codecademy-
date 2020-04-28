471e47209bc8203ccecce53b959d39989201097b
AbrahamPlazola <64374554+AbrahamPlazola@users.noreply.github.com>
2020-04-28 10:05:04 -0600

    Añadido objetos prueba

---
+++ Turismo/turismo.py
@@ -0,0 +1,2 @@
+destinos = ["Paris, Francia", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egipto"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
