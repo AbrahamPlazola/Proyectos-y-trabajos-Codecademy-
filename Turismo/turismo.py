187df64194457364a84a9f2b2a6ba8f685f2f7d7
AbrahamPlazola <64374554+AbrahamPlazola@users.noreply.github.com>
2020-04-28 10:20:10 -0600

    Creadas atracciones y funcion para añadir más

--- Turismo/turismo.py
+++ Turismo/turismo.py
@@ -1,8 +1,11 @@
 destinos = ["Paris, Francia", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egipto"]
-test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 def obtenerIndiceDestino(destino):
-    indiceDestino = destinos.index(destino)    
+    try:
+        indiceDestino = destinos.index(destino)
+    except ValueError:
+        print("Ese destino no existe")
+        indiceDestino = None   
     return indiceDestino
 
 def locacionViajero(viajero):
@@ -10,5 +13,25 @@ def locacionViajero(viajero):
     indiceDestinoViajero = obtenerIndiceDestino(destinoViaje)
     return indiceDestinoViajero
 
-test_indiceDestino = locacionViajero(test_traveler)
-print(test_indiceDestino)
+def añadirAtraccion(destino, atraccion):
+    indiceDestino = obtenerIndiceDestino(destino)
+    if indiceDestino == None:
+        return
+    atracciones[indiceDestino].append(atraccion)
+    return
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+atracciones = [[] for i in destinos]
+
+añadirAtraccion("Los Angeles, USA", ['Venice Beach', ['beach']])
+añadirAtraccion("Paris, Francia", ["the Louvre", ["art", "museum"]])
+añadirAtraccion("Paris, Francia", ["Arc de Triomphe", ["historical site", "monument"]])
+añadirAtraccion("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+añadirAtraccion("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+añadirAtraccion("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+añadirAtraccion("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+añadirAtraccion("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+añadirAtraccion("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+añadirAtraccion("Cairo, Egipto", ["Pyramids of Giza", ["monument", "historical site"]])
+añadirAtraccion("Cairo, Egipto", ["Egyptian Museum", ["museum"]])
