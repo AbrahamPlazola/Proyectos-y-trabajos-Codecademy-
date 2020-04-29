ef0e416de3df820d1dc0382076d80780a7bc2d2a
AbrahamPlazola <64374554+AbrahamPlazola@users.noreply.github.com>
2020-04-29 09:53:03 -0600

    Added buy and sell artowkr interactions

--- Standalones/ArtMarketplace.py
+++ Standalones/ArtMarketplace.py
@@ -23,7 +23,7 @@ class Marketplace:
         self.listings.append(newListing)
 
     def removeListing(self, toRemove):
-        self.listings.pop(toRemove)
+        self.listings.remove(toRemove)
 
     def showListings(self):
         for i in self.listings:
@@ -34,10 +34,49 @@ class Client:
         self.name = name
         self.locations = locations
         self.isMuseum = isMuseum
+
+    def sellArtwork(self, artwork, price):
+        if artwork.owner.name != self.name:
+            print("Error")
+            return
+
+        newListing = Listing(artwork, price, self.name)
+        veneer.addListing(newListing)
     
+    def buyArtwork(self, artwork):
+        if artwork.owner.name == self.name:
+            print("You already own that artwork")
+            return
+        
+        artListing = None
+        for i in veneer.listings:
+            if i.art == artwork:
+                artListing = i
+                break
+        if artListing != None:
+            artListing.art.owner = self
+            veneer.removeListing(artListing)
+        else:
+            print("The artwork is not on any listing")
+    
+class Listing:
+    def __init__(self, art, price, seller):
+        self.art = art
+        self.price = price
+        self.seller = seller
+    
+    def __repr__(self):
+        string = "{ArtName}, ${Price}".format(ArtName = self.art.title, Price = self.price)
+        return string
 
 veneer = Marketplace()
 edytta = Client("Edytta Halpirt")
 moma = Client("The MOMA", "New York", True)
 mandolinGirl = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", "oil on canvas", 1910, edytta)
+
+edytta.sellArtwork(mandolinGirl, "6M (USD)")
+# veneer.showListings()
+
+moma.buyArtwork(mandolinGirl)
 print(mandolinGirl)
+veneer.showListings()
